399be06e228635d13be3e671cce118f2cff12999
Nikolay <Nik.kovaliov.1987@mail.ru>
2023-02-20 22:04:41 +0300

    Добавили изменения в папку Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -11,7 +11,7 @@ int len = number.Length;
 
 if (len == 5)
 {
-   if (number[0] == number[4] || number[1] == number[3])
+   if (number[0] == number[4] && number[1] == number[3])
    {
       Console.WriteLine($"{number} - Да");
    }
